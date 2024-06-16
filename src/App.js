55360f7b57f27477ccbcc7237aa767440b4e724b
Titou-Dh <titoudh1@gmail.com>
2024-06-16 01:35:31 +0100

    added css

--- src/App.js
+++ src/App.js
@@ -55,16 +55,29 @@ const App = () => {
   };
 
   return (
+
     <div>
       <TicketForm onGenerate={generateTickets} />
-      {tickets.length > 0 && (
-        <PDFDownloadLink
-          document={<TicketPDF tickets={tickets} />}
-          fileName="tickets.pdf"
-        >
-          {({ loading }) => (loading ? 'Loading document...' : 'Download PDF')}
-        </PDFDownloadLink>
-      )}
+      <div class="flex p-4 mb-4 w-2/4 mx-auto my-7 text-sm text-blue-800 rounded-lg bg-blue-50  " role="alert">
+        <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <span class="sr-only">Info</span>
+        <div className='flex  items-center'>
+          <span class="font-medium">Clicker ici pour telecharger le pdf :</span>
+          
+          {tickets.length > 0 && (
+            <div className='text-blue-800 mx-3 bg-transparent border border-blue-800 hover:bg-blue-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-blue-600 dark:border-blue-600 dark:text-blue-400 dark:hover:text-white dark:focus:ring-blue-800'>
+            <PDFDownloadLink
+              document={<TicketPDF tickets={tickets} />}
+              fileName="tickets.pdf"
+            >
+              {({ loading }) => (loading ? 'Loading document...' : 'Download PDF')}
+            </PDFDownloadLink>
+          </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/TicketForm.js
+++ src/TicketForm.js
@@ -10,19 +10,38 @@ const TicketForm = ({ onGenerate }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h1 class="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <label>
-                Start Serial Number:
-                <input type="number" value={start} onChange={(e) => setStart(e.target.value)} />
-            </label>
-            <label>
-                End Serial Number:
-                <input type="number" value={end} onChange={(e) => setEnd(e.target.value)} />
-            </label>
-            <button type="submit" className='focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'>Generate Tickets</button>
+        <form onSubmit={handleSubmit} className="p-6 mt-32 max-w-md mx-auto bg-white rounded-xl shadow-md space-y-4">
+            <h1 className="text-3xl font-bold underline mb-4 text-center">Tickets printer</h1>
+            <div>
+                <label className="block text-sm font-medium text-gray-700">
+                    Start Serial Number:
+                </label>
+                <input
+                    type="number"
+                    value={start}
+                    onChange={(e) => setStart(e.target.value)}
+                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+            <div>
+                <label className="block text-sm font-medium text-gray-700">
+                    End Serial Number:
+                </label>
+                <input
+                    type="number"
+                    value={end}
+                    onChange={(e) => setEnd(e.target.value)}
+                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+            <div className='text-center'>
+                <button
+                    type="submit"
+                    className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                >
+                    Generate Tickets
+                </button>
+            </div>
         </form>
     );
 };
